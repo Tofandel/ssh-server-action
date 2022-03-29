c38edeb72f957638ae360bce1ca51eb38410002b
Tofandel <adrien.foulon@tukan.hu>
2022-03-29 16:20:41 +0200

    Fix script order

--- build/setup.tpl.sh
+++ build/setup.tpl.sh
@@ -5,15 +5,16 @@ sed -i -E "s|(AuthorizedKeysFile).*|\1 %h/.ssh/authorized_keys|g" /etc/ssh/sshd_
 echo "PubkeyAcceptedKeyTypes=+ssh-rsa" >> /etc/ssh/sshd_config
 
 HOME='/root'
+[ "$INPUT_USER_NAME" != "root" ] && HOME="/home/$INPUT_USER_NAME"
+
+[ "$INPUT_USER_NAME" != "root" ] && addgroup "$INPUT_USER_NAME" && adduser --shell /bin/ash --disabled-password --home "§HOME" --ingroup "$INPUT_USER_NAME" "$INPUT_USER_NAME"
+[ "$INPUT_USER_NAME" = "root" ] && sed -i -E "s/#?(PermitRootLogin).*/\1 yes/g" /etc/ssh/sshd_config
 
 mkdir -p "§HOME/.ssh/"
 (umask 066; touch "§HOME/.ssh/authorized_keys")
 
-[ "$INPUT_USER_NAME" != "root" ] && HOME="/home/$INPUT_USER_NAME"
 [ -n "$INPUT_PUBLIC_KEY" ] && echo "$INPUT_PUBLIC_KEY" >> "§HOME/.ssh/authorized_keys"
 [ -n "$INPUT_PUBLIC_KEY_URL" ] && wget -qO- "$INPUT_PUBLIC_KEY_URL" >> "§HOME/.ssh/authorized_keys"
-[ "$INPUT_USER_NAME" != "root" ] && addgroup "$INPUT_USER_NAME" && adduser --shell /bin/ash --disabled-password --home "§HOME" --ingroup "$INPUT_USER_NAME" "$INPUT_USER_NAME"
-[ "$INPUT_USER_NAME" = "root" ] && sed -i -E "s/#?(PermitRootLogin).*/\1 yes/g" /etc/ssh/sshd_config
 [ "$INPUT_SUDO_ACCESS" = "true" ] && sed -i -E "s/#? ?(%sudo.*?)ALL/\1NOPASSWD:ALL/g" /etc/sudoers && addgroup sudo && addgroup "$INPUT_USER_NAME" sudo
 [ -n "$INPUT_USER_PASSWORD" ] && sed -i -E "s/#?(ChallengeResponseAuthentication|PasswordAuthentication).*/\1 yes/g" /etc/ssh/sshd_config
 [ -z "$INPUT_USER_PASSWORD" ] && sed -i -E "s/#?(ChallengeResponseAuthentication|PasswordAuthentication).*/\1 no/g" /etc/ssh/sshd_config
