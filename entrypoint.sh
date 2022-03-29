057a2e1e2fe18750f93d3e8e52c28227f53882e6
Tofandel <adrien.foulon@tukan.hu>
2022-03-29 16:17:47 +0200

    Fix home interpolation

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 
 TAG=${GITHUB_SHA:-local}
 
-envsubst < build/setup.tpl.sh > build/setup.sh
-envsubst < build/entrypoint.tpl.sh > build/entrypoint.sh
+envsubst < build/setup.tpl.sh | sed -e 's/§/$/g' > build/setup.sh
+envsubst < build/entrypoint.tpl.sh | sed -e 's/§/$/g' > build/entrypoint.sh
 docker build -t "29a95e:$TAG" build
 docker run -d -p "$INPUT_PORT:22" --hostname "$INPUT_HOSTNAME" "29a95e:$TAG"
